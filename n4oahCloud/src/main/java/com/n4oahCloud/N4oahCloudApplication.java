cda5b6fcf0508a19af35a47c2bb5ebf2f32a4cfc
DESKTOP-43PFTP9\n4oah <n4oahdev@gmail.com>
2018-02-02 15:42:54 +0900

    no message

--- n4oahCloud/src/main/java/com/n4oahCloud/N4oahCloudApplication.java
+++ n4oahCloud/src/main/java/com/n4oahCloud/N4oahCloudApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class N4oahCloudApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(N4oahCloudApplication.class, args);
 	}
